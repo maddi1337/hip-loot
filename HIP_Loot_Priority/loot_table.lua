b56bf79cc60395db76af9227f63c629f2ffb799b
maddi1337 <mabrass@web.de>
2020-09-17 12:03:26 +0000

    09/17/20 12:03:26

--- HIP_Loot_Priority/loot_table.lua
+++ HIP_Loot_Priority/loot_table.lua
@@ -1,9 +1,9 @@
 hip_loot_table = {
 
 --### FULL LOOT PRIO HIP #####
-{["loot_id"] = "21837", ["loot_name"] = "Anubisath Warhammer", ["prio"] = " Prio: Ragnoz > Wurm(8) > Maddi(12) > Arthemius (7) > Hamma (5) > "},
+{["loot_id"] = "21837", ["loot_name"] = "Anubisath Warhammer", ["prio"] = " Prio: Ragnoz > Wurm(8) > Arthemius (7) > Hamma (5) > Nirenah > "},
 {["loot_id"] = "21836", ["loot_name"] = "Ritssyn's Ring of Chaos ", ["prio"] = " Prio: Baus  > Ester > Hankmoody > Greun > Talatro > "},
-{["loot_id"] = "21891", ["loot_name"] = "Shard of the Fallen Star ", ["prio"] = " Prio: "},
+{["loot_id"] = "21891", ["loot_name"] = "Shard of the Fallen Star ", ["prio"] = " Prio: Kendy > "},
 {["loot_id"] = "20928", ["loot_name"] = "Qiraji Bindings of Command", ["prio"] = " Prio: Kruzade  > Arthemius (11) > Cammy(7) >  Hamma(6) > Ailenya(6) > "},
 {["loot_id"] = "20932", ["loot_name"] = "Qiraji Bindings of Dominance", ["prio"] = " Prio: Greun > Vulkania > Kendy > "},
 {["loot_id"] = "20930", ["loot_name"] = "Vek'lor's Diadem", ["prio"] = " Prio: Ninjawodja > Hamma (8) >  VodkaO (4) > Jomo (8) > "},
