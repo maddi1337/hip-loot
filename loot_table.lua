6f03d2d32419e682e9327efa4c76867dfab9cb29
Starbuzz83 <cubie@ubtu>
2020-09-24 03:00:05 +0200

    09/24/20 03:00:05

--- loot_table.lua
+++ loot_table.lua
@@ -1,7 +1,7 @@
 hip_loot_table = {
 
 --### FULL LOOT PRIO HIP #####
-{["loot_id"] = "21837", ["loot_name"] = "Anubisath Warhammer", ["prio"] = " Prio: Wurm(8) > Maddi(12) > Hamma (5) > Nirenah > Kruzade > "},
+{["loot_id"] = "21837", ["loot_name"] = "Anubisath Warhammer", ["prio"] = " Prio: Wurm(9) > Maddi(12) > Hamma (5) > Nirenah > Kruzade > "},
 {["loot_id"] = "21836", ["loot_name"] = "Ritssyn's Ring of Chaos ", ["prio"] = " Prio: Greun > Ester > Hankmoody > Baus > Talatro > "},
 {["loot_id"] = "21891", ["loot_name"] = "Shard of the Fallen Star ", ["prio"] = " Prio: "},
 {["loot_id"] = "20928", ["loot_name"] = "Qiraji Bindings of Command", ["prio"] = " Prio: Ailenya(6) > Mortal(11) > Ninjawodja > Kruzade  > Maddi(19) > "},
@@ -40,7 +40,7 @@ hip_loot_table = {
 {["loot_id"] = "21677", ["loot_name"] = "Ring of the Qiraji Fury", ["prio"] = " Prio: Seyt(1) > Wurm(5) > Ragefire(7) > Maddi(6) > Baldemar (3) > "},
 {["loot_id"] = "22399", ["loot_name"] = "Idol of Health", ["prio"] = " Prio: If > Pvpgott > "},
 {["loot_id"] = "21617", ["loot_name"] = "Wasphide Gauntlets", ["prio"] = " Prio: Pvpgott > "},
-{["loot_id"] = "21618", ["loot_name"] = "Hive Defiler Wristguards", ["prio"] = " Prio: Maddi(4) > Wurm(4) > Thranng(6) > Nirenah > Ragnoz > "},
+{["loot_id"] = "21618", ["loot_name"] = "Hive Defiler Wristguards", ["prio"] = " Prio: Wurm(4) > Thranng(6) > Nirenah > Ragnoz > Kruzade > "},
 {["loot_id"] = "21621", ["loot_name"] = "Cloak of the Golden Hive", ["prio"] = " Prio: Hayze > Kruzade > Nirenah > Ragnoz > "},
 {["loot_id"] = "21598", ["loot_name"] = "Royal Qiraji Belt", ["prio"] = " Prio: Nirenah > Kruzade > Hayze > "},
 {["loot_id"] = "21597", ["loot_name"] = "Royal Scepter of Vek'lor", ["prio"] = " Prio: Vulkania > Cz > Starbuzz > Greun > Baus > "},
