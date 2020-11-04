3c0a23d5e27cbc5422371b1454ace59f1eb1daa8
Starbuzz83 <cubie@ubtu>
2020-11-04 23:00:04 +0100

    11/04/20 23:00:04

--- loot_table.lua
+++ loot_table.lua
@@ -57,7 +57,7 @@ hip_loot_table = {
 {["loot_id"] = "21615", ["loot_name"] = "Don Rigoberto's Lost Hat", ["prio"] = " Prio: Elfierde > Enymen > Blargus > Toro > If > "},
 {["loot_id"] = "23557", ["loot_name"] = "Larvae of the Great Worm", ["prio"] = " Prio: Seyt(4) > Thranng (9) > Cammy (11) > Hamma (12) > Alvaroh (?) > "},
 {["loot_id"] = "23570", ["loot_name"] = "Jom Gabbar", ["prio"] = " Prio: Alvaroh (3) > Ailenya (3) > Cammy (4) > Seyt(7) > Mortal (14) > "},
-{["loot_id"] = "21221", ["loot_name"] = "Eye of C'thun", ["prio"] = " Prio: Phizzy > Kendy > If > Ronerva > Alvaroh (9) > "},
+{["loot_id"] = "21221", ["loot_name"] = "Eye of C'thun", ["prio"] = " Prio: Kendy > If > Ronerva > Alvaroh (9) > Kruzade > "},
 {["loot_id"] = "21126", ["loot_name"] = "Death's Sting", ["prio"] = " Prio: Mortal (1) > Nirenah > Ninja > Hamma (20) > Cammy (20) > "},
 {["loot_id"] = "21134", ["loot_name"] = "Dark Edge of Insanity", ["prio"] = " Prio: Seyt(9) > Thranng (8) > "},
 {["loot_id"] = "21581", ["loot_name"] = "Gauntlets of Annihilation", ["prio"] = " Prio: Ragnoz > Nirenah > Kruzade > Hayze > "},
@@ -68,7 +68,7 @@ hip_loot_table = {
 {["loot_id"] = "21596", ["loot_name"] = "Ring of the Godslayer", ["prio"] = " Prio: Wurm > Maddi > "},
 {["loot_id"] = "21839", ["loot_name"] = "Scepter of the False Prophet", ["prio"] = " Prio: Elfierde > Para > Ronerva > Anyph > Methy > "},
 {["loot_id"] = "22730", ["loot_name"] = "Eyestalk Waist Cord", ["prio"] = " Prio: Hankmoody > Cz > Talatro > Starbuzz > Ester > "},
-{["loot_id"] = "22731", ["loot_name"] = "Cloak of the Devoured", ["prio"] = " Prio: Baus > Greun > Phizzy > Cz > Starbuzz > "},
+{["loot_id"] = "22731", ["loot_name"] = "Cloak of the Devoured", ["prio"] = " Prio: Baus > Greun > Cz > Starbuzz > Kendy > "},
 {["loot_id"] = "22732", ["loot_name"] = "Mark of C'Thun", ["prio"] = " Prio: Hayze > Ragnoz > Nirenah > "},-- #############################
 {["loot_id"] = "20619", ["loot_name"] = "Acid Inscribed Greaves", ["prio"] = " Prio: Notes Not Found",},
 {["loot_id"] = "20637", ["loot_name"] = "Acid Inscribed Pauldrons", ["prio"] = " Prio: Notes Not Found",},
