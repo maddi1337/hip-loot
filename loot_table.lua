69de603b5bddcab71ab47ec5ce5e6dcf3fb2d96e
Starbuzz83 <cubie@ubtu>
2021-01-09 15:00:05 +0100

    01/09/21 15:00:05

--- loot_table.lua
+++ loot_table.lua
@@ -86,7 +86,7 @@ hip_loot_table = {
 {["loot_id"] = "22362", ["loot_name"] = "Desecrated Wristguards", ["prio"] = " Prio: Scorê(6) > Flüffel(11) > Asphalaen(15) > If(9) > Anyph(12) > "},
 {["loot_id"] = "22355", ["loot_name"] = "Desecrated Bracers", ["prio"] = " Prio: Elî(4) > Hayze(12) > Melfice (5) > Vancleeves (10) > Hamma (10) > "},
 {["loot_id"] = "22371", ["loot_name"] = "Desecrated Gloves", ["prio"] = " Prio: Rezzer (2) > Ilarion (7) > Venaa (9) > Brainheal (9) > Ester (12) > "},
-{["loot_id"] = "22364", ["loot_name"] = "Desecrated Handguards", ["prio"] = " Prio: Patboss(-) > Scorê(14) > "},
+{["loot_id"] = "22364", ["loot_name"] = "Desecrated Handguards", ["prio"] = " Prio: Flüffel(12) > Patboss(-) > Scorê(14) > "},
 {["loot_id"] = "22357", ["loot_name"] = "Desecrated Gauntlets", ["prio"] = " Prio: Ailenya (5) > Ragnoz(6) > Alvaroh (8) > Alvaroh (8) > Cammy (9) > "},
 {["loot_id"] = "22368", ["loot_name"] = "Desecrated Shoulderpads", ["prio"] = " Prio: Nitilas (8) > Enyman (6) > Ilarion (8) > Rezzer (6) > Venaa (6) > "},
 {["loot_id"] = "22361", ["loot_name"] = "Desecrated Spaulders", ["prio"] = " Prio: Pvpgott(6) > Scorê(9) > Toro(-) > If(15) > Patboss(8) > "},
