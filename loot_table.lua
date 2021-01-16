0d2471b7a4d20dcd37f39857f01f5e9a0de09ea7
Starbuzz83 <cubie@ubtu>
2021-01-16 20:00:05 +0100

    01/16/21 20:00:05

--- loot_table.lua
+++ loot_table.lua
@@ -114,7 +114,7 @@ hip_loot_table = {
 {["loot_id"] = "22943", ["loot_name"] = "Malice Stone Pendant", ["prio"] = " Prio: "},
 {["loot_id"] = "22941", ["loot_name"] = "Polar Shoulder Pads", ["prio"] = " Prio: "},
 {["loot_id"] = "22940", ["loot_name"] = "Icebane Pauldrons", ["prio"] = " Prio: "},
-{["loot_id"] = "22942", ["loot_name"] = "The Widow's Embrace", ["prio"] = " Prio: Para (-) > Ilarion > Toro (3) > If (12) > Patboss (-) > "},
+{["loot_id"] = "22942", ["loot_name"] = "The Widow's Embrace", ["prio"] = " Prio: Para (-) > Ilarion > Toro (3) > Rezzer > If (12) > "},
 {["loot_id"] = "22806", ["loot_name"] = "Widow's Remorse", ["prio"] = " Prio: Elî(2) > Ragnoz(10) > Hamma (9) > "},
 {["loot_id"] = "22947", ["loot_name"] = "Pendant of Forgotten Names", ["prio"] = " Prio: "},
 {["loot_id"] = "23220", ["loot_name"] = "Crystal Webbed Robe", ["prio"] = " Prio: "},
