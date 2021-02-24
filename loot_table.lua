5e2cb0224370ff72c2293fde5a69260fbfbf1ca6
Starbuzz83 <cubie@ubtu>
2021-02-24 02:00:07 +0100

    02/24/21 02:00:07

--- loot_table.lua
+++ loot_table.lua
@@ -73,7 +73,7 @@ hip_loot_table = {
 {["loot_id"] = "22731", ["loot_name"] = "Cloak of the Devoured", ["prio"] = " Prio: Greun > Starbuzz > Nitilas > Kendy > Talatro > "},
 {["loot_id"] = "22732", ["loot_name"] = "Mark of C'Thun", ["prio"] = " Prio: Ragnoz > Nirenah > Maddi > Wurm > "},
 {["loot_id"] = "23667", ["loot_name"] = "Spaulders of the Grand Crusader", ["prio"] = " Prio: "},
-{["loot_id"] = "23069", ["loot_name"] = "Necro-Knight's Garb", ["prio"] = " Prio: "},
+{["loot_id"] = "23069", ["loot_name"] = "Necro-Knight's Garb", ["prio"] = " Prio: Cz (-) > "},
 {["loot_id"] = "23226", ["loot_name"] = "Ghoul Skin Tunic", ["prio"] = " Prio: "},
 {["loot_id"] = "23663", ["loot_name"] = "Girdle of Elemental Fury", ["prio"] = " Prio: "},
 {["loot_id"] = "23666", ["loot_name"] = "Belt of the Grand Crusader", ["prio"] = " Prio: "},
